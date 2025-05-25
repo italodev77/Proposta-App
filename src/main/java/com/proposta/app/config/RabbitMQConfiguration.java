98143ac1a2f0c8a9134e9979aff0a5cdfb26c3bf
devitalo <italo_ariel_dev77@outlook.com>
2025-05-25 02:07:32 -0300

    feat: implementado a configuração da exchange proposta pendente e refatoração do código para resgatar o valor dessa exchange pelo o arquivo application.yml, assim seguindo boas práticas

--- src/main/java/com/proposta/app/config/RabbitMQConfiguration.java
+++ src/main/java/com/proposta/app/config/RabbitMQConfiguration.java
@@ -3,6 +3,9 @@ package com.proposta.app.config;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +13,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
+
+    @Value("${spring.rabbitmq.propostaPendente.exchange}")
+    private String exchange;
 
     @Bean
     public Queue criarFilaPropostaPendenteMsAnaliseCredito(){
@@ -41,7 +47,7 @@ public class RabbitMQConfiguration {
 
     @Bean
     public FanoutExchange criarFanoutExchangePropostaPendente(){
-        return ExchangeBuilder.fanoutExchange("proposta-pendente.ex").build();
+        return ExchangeBuilder.fanoutExchange(exchange).build();
     }
 
     @Bean
@@ -54,5 +60,18 @@ public class RabbitMQConfiguration {
     public Binding criarBindingPropostaPendenteMsNotificacao(){
         return BindingBuilder.bind(criarFilaPropostaPendenteMsNotificacao()).
                 to(criarFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
     }
 }
--- src/main/java/com/proposta/app/service/PropostaService.java
+++ src/main/java/com/proposta/app/service/PropostaService.java
@@ -7,24 +7,37 @@ import com.proposta.app.entity.Proposta;
 import com.proposta.app.mapper.PropostaMapper;
 import com.proposta.app.repository.PropostaRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@AllArgsConstructor
+
 @Service
 public class PropostaService {
+
 
     private PropostaRepository propostaRepository;
 
     private NotificacaoService notificacaoService;
+
+    @Value("${spring.rabbitmq.propostaPendente.exchange}")
+    private String exchange;
+
+    public PropostaService(PropostaRepository propostaRepository, NotificacaoService notificacaoService, @Value("${spring.rabbitmq.propostaPendente.exchange}") String exchange) {
+        this.propostaRepository = propostaRepository;
+        this.notificacaoService = notificacaoService;
+        this.exchange = exchange;
+    }
+
+
 
     public PropostaResponseDTO criar(PropostaRequestDTO requestDTO){
         Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(requestDTO);
         propostaRepository.save(proposta);
 
         PropostaResponseDTO response = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
-        notificacaoService.notificar(response,"proposta-pendente.ex");
+        notificacaoService.notificar(response,exchange);
 
         return response;
 
